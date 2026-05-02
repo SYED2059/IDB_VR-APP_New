794877e3269e248ce88a4fe39e5b8d6f1b68ca67
Siva <sivanamachivayam87@gmail.com>
2026-05-02 07:55:30 +0530

    Flash Screen added.

---
+++ vr/Assets/Scripts/FlashManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class FlashManager : MonoBehaviour
+{
+    public VideoPlayer FlashvideoPlayer;
+    public VideoClip FlashClip;
+
+    void Start()
+    {
+        FlashvideoPlayer.waitForFirstFrame = true;
+        FlashvideoPlayer.loopPointReached += OnFlashVideoFinished;
+    }
+
+    void OnFlashVideoFinished(VideoPlayer vp)
+    {
+        if (vp.clip == FlashClip)
+        {
+            Debug.Log("FlashVideoFinished");
+            SceneManager.LoadScene("GameScene");
+        }
+    }
+}
